0509c12def957bd374ab1e140cb5a0c2bdbbf669
loviewer <tomatoo3o97@gmail.com>
2022-04-11 17:51:21 +0900

    spring project insert

---
+++ backend/kh-final-popcon/src/main/java/com/popcon/khfinalpopcon/KhFinalPopconApplication.java
@@ -0,0 +1,13 @@
+package com.popcon.khfinalpopcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhFinalPopconApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhFinalPopconApplication.class, args);
+    }
+
+}
