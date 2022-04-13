f3c2515868ac1bb3cd08d18034ed1adf17a82b51
2-seulgi <90025449+2-seulgi@users.noreply.github.com>
2022-04-13 18:13:00 +0900

    헤더 수정

--- react/final_v4_test/src/components/Header.js
+++ react/final_v4_test/src/components/Header.js
@@ -2,32 +2,32 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import OpenMenu from './OpenMenu';
 import { slide as Menu } from 'react-burger-menu';
-import { Navbar, Container, Offcanvas, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src="./img/popcon_logo_test.png"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
             <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
+              <Nav.Link as={Link} to="/mainPage">
+                Home
               </Nav.Link>
+              <Nav.Link as={Link} to="/explorePage">
+                Explore
+              </Nav.Link>
+              <Nav.Link as={Link} to="/likePage">Like</Nav.Link>
+              <Nav.Link as={Link} to="/myPage">Mypage</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
